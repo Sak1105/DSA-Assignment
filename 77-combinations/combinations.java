f87e9c5dbc6dae4339514fbfc471b15764e63bd9
Sak1105 <sakshiyadav11052005@gmail.com>
2025-10-22 22:34:15 +0530

    Time: 18 ms (77.12%) | Memory: 94.6 MB (78.86%) - LeetSync

---
+++ 77-combinations/combinations.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        findCombinations(1, n, k, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void findCombinations(int start, int n, int k, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == k) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            current.add(i); 
+            findCombinations(i + 1, n, k, current, result);  
+            current.remove(current.size() - 1);  
+        }
+    }
+}
