dd0ddea1388cab4e8de7d7ec1b96f8cb24f02520
Sak1105 <sakshiyadav11052005@gmail.com>
2025-12-24 01:16:51 +0530

    Time: 1 ms (16.98%) | Memory: 49 MB (6.91%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int l=findLast(nums,target,nums.length-1);
+        int f=findFirst(nums,target,0);
+        int[] res={f,l};
+        return res;
+    }
+    private static int findFirst(int[] nums, int target, int pos){
+        if(pos==nums.length)
+        {
+            return -1;
+        }
+        if(nums[pos]==target){
+            return pos;
+        }
+        return findFirst(nums,target,pos+1);
+    }
+    private static int findLast(int[] nums, int target, int pos){
+        if(pos==-1)
+        {
+            return -1;
+        }
+        if(nums[pos]==target){
+            return pos;
+        }
+        return findLast(nums,target,pos-1);
+    }
+}
