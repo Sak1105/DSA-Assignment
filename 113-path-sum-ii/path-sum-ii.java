e3d8e41bcfbee12c900131f5d7c5c197bea540d4
Sak1105 <sakshiyadav11052005@gmail.com>
2026-04-16 01:45:01 +0530

    Time: 1 ms (100.00%) | Memory: 45.5 MB (43.42%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -16,26 +16,28 @@
 import java.util.*;
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
-        dfs(root, targetSum, path, result);
-        return result;
+       List<List<Integer>> ans=new ArrayList<>();
+       List<Integer> arr=new ArrayList<>();
+       helper(ans,arr,root,targetSum);
+       return ans;
     }
-    private void dfs(TreeNode node, int remainingSum,
-                     List<Integer> path,
-                     List<List<Integer>> result) {
-        if (node == null) {
+    private static void helper(List<List<Integer>> ans,List<Integer> arr,TreeNode root, int sum ){
+        if(root==null){
             return;
         }
-        path.add(node.val);
-        if (node.left == null && node.right == null && remainingSum == node.val) {
-            result.add(new ArrayList<>(path)); // store a copy
-        } 
-        else 
-        {
-            dfs(node.left, remainingSum - node.val, path, result);
-            dfs(node.right, remainingSum - node.val, path, result);
+         arr.add(root.val);
+        if(root.left==null && root.right==null){
+            if(root.val==sum){
+                ans.add(new ArrayList<>(arr));
+            }
+            arr.remove(arr.size()-1);
+            return;
         }
-        path.remove(path.size() - 1);
+
+        helper(ans, arr,root.left, sum-root.val);
+        helper(ans, arr,root.right, sum-root.val);
+        arr.remove(arr.size()-1);
+        return;
+    
     }
 }
